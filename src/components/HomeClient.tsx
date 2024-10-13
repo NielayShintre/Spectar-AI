c000b53a10cb0802a8131f9a1b76329e9c0551be
Nielay Shintre <nielayshintre18@gmail.com>
2024-10-13 23:41:22 +0530

    added icons

--- src/components/HomeClient.tsx
+++ src/components/HomeClient.tsx
@@ -4,7 +4,7 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-import { ArrowRight, LogIn, Loader2 } from "lucide-react";
+import { ArrowRight, LogIn, Loader2, FileText, Scale } from "lucide-react";
 import Link from "next/link";
 import FileUpload from "@/components/ui/FileUpload";
 import UpgradeModal from "@/components/UpgradeModal";
@@ -116,14 +116,20 @@ export default function HomeClient({
   return (
     <div className="relative w-screen min-h-screen overflow-hidden bg-gradient-to-r from-blue-500 to-slate-300">
       <div className="relative z-10 flex items-center justify-center min-h-screen">
-      <div className="absolute top-4 left-1/2 transform -translate-x-1/2">
-        <h1 className="text-4xl font-bold text-blue-900">⚖ Spectar AI</h1>
-      </div>
+        <div className="absolute top-4 left-1/2 transform -translate-x-1/2 flex items-center">
+          <Scale className="w-12 h-12 mr-3 text-blue-900" />
+          <h1 className="text-4xl font-bold text-blue-900">Spectar AI</h1>
+        </div>
         <div className="max-w-4xl p-8 bg-white bg-opacity-90 rounded-lg shadow-xl">
           <div className="flex flex-col items-center text-center">
+            {/* <div className="flex items-center mb-6">
+              <FileText className="w-12 h-12 text-blue-500 mr-4" />
+            </div> */}
             <div className="flex items-center">
+              <FileText className="w-12 h-12 text-blue-600 mr-3" />
               <h1 className="mr-3 text-3xl font-bold text-gray-800">
-                <span className="text-blue-600">Chat with</span> with any Legal Document
+                <span className="text-blue-600">Chat with</span> any Legal
+                Document
               </h1>
               <UserButton afterSwitchSessionUrl="/" />
             </div>
